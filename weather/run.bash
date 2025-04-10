c10307675d3833c14441153318f6a434c4e031a7
infoF <franzmueller343@gmail.com>
2025-04-10 16:36:59 +0200

    testrun

--- weather/run.bash
+++ weather/run.bash
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# echo $GOOGLE_SHEET
-# echo $GOOGLE_WORK_SHEET
-# echo $STATION_NAME
-
 DOWNLOADFILENAME="./wetter.py"
 
-# curl \
-#     -H "Authorization: token ${PAT}" \
-#     -H 'Accept: application/vnd.github.v3.raw' \
-#     -s \
-#     -o $DOWNLOADFILENAME \
-#     -L https://raw.githubusercontent.com/infoF/WeatherIgling/master/wetter_igling_gs.py
+curl \
+    -H "Authorization: token ${PAT}" \
+    -H 'Accept: application/vnd.github.v3.raw' \
+    -s \
+    -o $DOWNLOADFILENAME \
+    -L https://raw.githubusercontent.com/infoF/WeatherIgling/master/wetter_igling_gs.py
 
-python ./weather/test_run.py --in $STATION_NAME
+python $DOWNLOADFILENAME --station-name $STATION_NAME  --google-sheet $GOOGLE_SHEET  --google-work-sheet $GOOGLE_WORK_SHEET --use_env "true"
