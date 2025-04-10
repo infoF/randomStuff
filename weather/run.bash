0c7a27866cec255f6fb635667b3853790e02c018
infoF <franzmueller343@gmail.com>
2025-04-10 16:21:58 +0200

    alter input

--- weather/run.bash
+++ weather/run.bash
@@ -6,11 +6,11 @@ echo $STATION_NAME
 
 DOWNLOADFILENAME="./wetter.py"
 
-curl \
-    -H "Authorization: token ${PAT}" \
-    -H 'Accept: application/vnd.github.v3.raw' \
-    -s \
-    -o $DOWNLOADFILENAME \
-    -L https://raw.githubusercontent.com/infoF/WeatherIgling/master/wetter_igling_gs.py
+# curl \
+#     -H "Authorization: token ${PAT}" \
+#     -H 'Accept: application/vnd.github.v3.raw' \
+#     -s \
+#     -o $DOWNLOADFILENAME \
+#     -L https://raw.githubusercontent.com/infoF/WeatherIgling/master/wetter_igling_gs.py
 
-ls -l
+python ./weather/test_run.py --in "111"
